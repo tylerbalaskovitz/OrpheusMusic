8e054e55bcc48adff9ffed16cb9da154d10b1bd2
Alex Proctor <taproctor00@gmail.com>
2022-05-31 16:11:15 -0400

    finished registration backend, fixed login backend

--- backend/src/main/java/com/revature/repositories/orpheus_user_repository.java
+++ backend/src/main/java/com/revature/repositories/orpheus_user_repository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.revature.models.orpheus_user;
 
 @Repository
-public interface orpheus_user_repository extends JpaRepository<orpheus_user,Long>{
+public interface orpheus_user_repository extends JpaRepository<orpheus_user,Long> {
 	public orpheus_user findByUsername(String username);
+	public boolean existsByUsername(String username);
 }
